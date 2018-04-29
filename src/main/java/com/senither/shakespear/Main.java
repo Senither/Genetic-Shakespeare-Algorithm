ed478529d31c22d43d10cfa90731aced1fabb520
Alexis Tan <alexis@sen-dev.com>
2018-04-29 16:40:39 +0200

    Create loop to go over the full sonnet on letter at a time

--- src/main/java/com/senither/shakespear/Main.java
+++ src/main/java/com/senither/shakespear/Main.java
@@ -5,8 +5,50 @@ public class Main {
     private static final int ROUNDS = 1000;
 
     public static void main(String[] args) {
-        String sonnet = "To be or not to be.";
+        String fullSonnet = "To be, or not to be, that is the question:\n" +
+                "Whether 'tis nobler in the mind to suffer\n" +
+                "The slings and arrows of outrageous fortune,\n" +
+                "Or to take arms against a sea of troubles\n" +
+                "And by opposing end them. To die—to sleep,\n" +
+                "No more; and by a sleep to say we end\n" +
+                "The heart-ache and the thousand natural shocks\n" +
+                "That flesh is heir to: 'tis a consummation\n" +
+                "Devoutly to be wish'd. To die, to sleep;\n" +
+                "To sleep, perchance to dream—ay, there's the rub:\n" +
+                "For in that sleep of death what dreams may come,\n" +
+                "When we have shuffled off this mortal coil,\n" +
+                "Must give us pause—there's the respect\n" +
+                "That makes calamity of so long life.\n" +
+                "For who would bear the whips and scorns of time,\n" +
+                "Th'oppressor's wrong, the proud man's contumely,\n" +
+                "The pangs of dispriz'd love, the law's delay,\n" +
+                "The insolence of office, and the spurns\n" +
+                "That patient merit of th'unworthy takes,\n" +
+                "When he himself might his quietus make\n" +
+                "With a bare bodkin? Who would fardels bear,\n" +
+                "To grunt and sweat under a weary life,\n" +
+                "But that the dread of something after death,\n" +
+                "The undiscovere'd country, from whose bourn\n" +
+                "No traveller returns, puzzles the will,\n" +
+                "And makes us rather bear those ills we have\n" +
+                "Than fly to others that we know not of?\n" +
+                "Thus conscience does make cowards of us all,\n" +
+                "And thus the native hue of resolution\n" +
+                "Is sicklied o'er with the pale cast of thought,\n" +
+                "And enterprises of great pitch and moment\n" +
+                "With this regard their currents turn awry\n" +
+                "And lose the name of action.";
 
+
+        StringBuilder sonnet = new StringBuilder();
+        for (int i = 0; i < fullSonnet.length(); i++) {
+            sonnet.append(fullSonnet.charAt(i));
+
+            run(sonnet.toString());
+        }
+    }
+
+    public static void run(String sonnet) {
         System.out.println("Running generic algorithm to try and learn to write the following sentence:\n");
         System.out.println("\t" + sonnet);
         System.out.println("\nRunning populations...");
@@ -14,7 +56,8 @@ public class Main {
         int generations = 0;
         int jump = 100;
         int next = jump - 1;
-        long milliseconds = System.currentTimeMillis();
+        long start = System.currentTimeMillis();
+        long milliseconds = start;
         for (int i = 0; i < ROUNDS; i++) {
             if (i % jump == 0 || i == 0) {
                 System.out.print("Running population #" + i + " - #" + (i + jump));
@@ -37,7 +80,10 @@ public class Main {
                 milliseconds = System.currentTimeMillis();
             }
         }
+        long total = System.currentTimeMillis() - start;
+
         System.out.println("Done!\n");
-        System.out.println("It took an average of " + (generations / ROUNDS) + " generations to get to the end result");
+        System.out.println("It took an average of " + (generations / ROUNDS) + " generations to get to the end result.");
+        System.out.println("Total time taken was " + (total) + " ms (An average of " + (total / ROUNDS) + " ms each population)");
     }
 }
