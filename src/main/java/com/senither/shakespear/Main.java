75311452504f9e097b6eb36e361200f8bd5d8e73
Alexis Tan <alexis@sen-dev.com>
2018-04-29 14:42:25 +0200

    Initial commit

---
+++ src/main/java/com/senither/shakespear/Main.java
@@ -0,0 +1,10 @@
+package com.senither.shakespear;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String sonnet = "To be or not to be.";
+
+        System.out.println(sonnet);
+    }
+}
